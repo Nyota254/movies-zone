a9c1b893e494ea0ba62cf66a0dc6dc7d4b133bb8
Nyota254 <knyota66@gmail.com>
2019-09-03 12:40:29 +0300

    add a for loop and macros file

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 
 #Views
 @app.route('/')
@@ -7,8 +8,15 @@ def index():
     '''
     View root page that returns the index page and its data
     '''
+
+    #Getting the popular movies
+    popular_movies = get_movies('popular')
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
+    print(popular_movies)
+
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html',title = title)
+    return render_template('index.html',title = title,popular = popular_movies,upcoming = upcoming_movie,now_showing =now_showing_movie)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
@@ -17,3 +25,6 @@ def movie(movie_id):
     '''
     title = f'You are viewing {movie_id}'
     return render_template('movie.html',title = title)
+
+
+
